b65fbec60774d4ec5f28a01cf195c66d343671f1
j_c_cook <jack.cook@okstate.edu>
2021-02-05 14:34:44 -0600

    a file for generating coordinates and converting the cpgfunction outputs into library.json files

---
+++ gFunctionLibrary/coordinate_generator.py
@@ -0,0 +1,7 @@
+# Friday, February 5, 2021
+
+"""
+**coordinate_generator.py**
+
+A module that will contain the functions and classes associated with generating x,y coordinates
+"""
---
+++ gFunctionLibrary/folder_to_lib.py
@@ -0,0 +1,8 @@
+# Friday, February 5, 2020
+
+"""
+**folder_to_lib.py**
+
+This module will be where the functions for taking path/to/folder outputs from cpgfunction are combined
+into a library.json file
+"""
