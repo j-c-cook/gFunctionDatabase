791d9ed4b970dccff5d8e7a3d1a2bc3635c662c4
j-c-cook <jack.cook@okstate.edu>
2021-08-05 09:54:11 -0500

    Update data.available example function

--- gFunctionDatabase/Data/available.py
+++ gFunctionDatabase/Data/available.py
@@ -50,7 +50,7 @@ def find_data_files(file_ext='json'):
     ---------
     >>> import gFunctionDatabase as gfdb
     >>> path_to_database, available_data_files = \
-        gfdb.Database.available.find_data_files()
+        gfdb.Data.available.find_data_files()
     """
     # get forward or backward slash based on OS
     slash = General.platform_specific.get_slash_style()
